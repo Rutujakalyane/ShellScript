cd801bcdb9a1dd3f928bfef5691be40c472f4d92
rutuja kalyane <you@example.com>
2020-03-24 14:40:58 +0530

    ADDED:Flip coin till either Heads/Tails 11 times

---
+++ WhileLoop/FlipCoin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+heads=0
+tails=0
+while [[$heads -lt 11 && $tails -lt 11]] 
+do
+	coin=$(($RANDOM % 2))
+	if [$coin -eq 1]
+	then
+		((heads++))
+	else
+		((tails++))
+	fi
+done
+echo heads = $heads
+echo tails = $tails
---
+++ WhileLoop/TableOfPower2.sh
@@ -0,0 +1,13 @@
+read -p "Enter Number : " num
+power=1
+count=1
+while [ $count -le $num ]
+do
+	#checking if less than 256
+	if [ $power -le 256 ]
+	then
+		echo $power
+		power=$(($power*2))
+	fi
+	((count++))
+done
