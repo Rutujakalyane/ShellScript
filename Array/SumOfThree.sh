2169a0362f7a049092101c12661ff1e33fa3b7cd
rutuja kalyane <you@example.com>
2020-03-26 19:58:33 +0530

    ADDED: Check if sum of three digits adds to zero

--- Array/SumOfThree.sh
+++ Array/SumOfThree.sh
@@ -1,8 +1,29 @@
-read -p "Enter three numbers : " num[0] num[1] num[2]
-sum=$((${num[0]}+${num[1]}+${num[2]}))
-if [ $sum -eq 0 ]
-then
-        echo "Three numbers adds to zero"
-else
-        echo "Three numbers adds to non Zero"
-fi
+read -p "Enter size of array:" size
+echo "Enter elements in array:"
+counter=1
+
+for (( i=0; i<$size; i++ ))
+do
+	read -p "Enter: " number
+	array[i]=$number
+done
+echo ${array[@]}
+
+
+for ((first=0; first<$(($size-2)); first++ ))
+do
+	for (( second=$((first+1)); second<$(($size-1));  second++ ))
+	do
+		for (( third=$((second+1)); third<$size; third++ ))
+		do
+			if [ $(( array[$first] + array[$second] + array[$third])) -eq 0 ]
+			then
+				echo "triplate: " $counter
+				echo " ${array[$first]} "
+				echo " ${array[$second]} "
+				echo " ${array[third]}"
+				((counter++))
+			fi
+		done
+	done
+done
