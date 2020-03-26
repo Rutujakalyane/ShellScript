27656dac09d865415d8d09b46c44a29f165dd53d
rutuja kalyane <you@example.com>
2020-03-26 16:12:47 +0530

    ADDED: Check number is Palindrome or not

---
+++ functions/Palindromes.sh
@@ -0,0 +1,20 @@
+function palindromes(){
+number=$n
+reverse=""
+while [ $n -gt 0 ]
+do
+        reminder=$(($n % 10))
+        reverse="$reverse$reminder"
+        n=$(($n / 10))
+done
+        echo $reverse
+        if [ $number -eq $reverse ]
+        then
+                echo "Number is palindrome"
+        else
+                echo "Number is not palindrome"
+
+        fi
+}
+                read -p "Enter the number  : " n
+                palindromes $n
